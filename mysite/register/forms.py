dde821d6050c75fb5d33c68e42af53a38adaaf32
Mannat Kaur <mannatk@gmail.com>
2020-04-13 17:55:05 -0700

    add lib card field to forms

--- mysite/register/forms.py
+++ mysite/register/forms.py
@@ -4,14 +4,17 @@ from django.contrib.auth.models import User
 
 class NewUserForm(UserCreationForm):
     email = forms.EmailField(required=True)
-
+    library_card_number = forms.IntegerField(required=True)
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("username", "email", "library_card_number", "password1", "password2",)
 
     def save(self, commit=True):
         user = super(NewUserForm, self).save(commit=False)
         user.email = self.cleaned_data["email"]
+        user.library_card = self.cleaned_data['library_card']
         if commit:
             user.save()
         return user
+
+
