1efa212c1d093468c83fef884303b31fd0da85b3
Mannat Kaur <mannatk@gmail.com>
2020-04-18 22:08:46 -0700

    Added tests for sign up page

--- mysite/register/tests.py
+++ mysite/register/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .forms import NewUserForm
+from django.contrib.auth.forms import UserCreationForm
+# Create your tests here. 
 
-# Create your tests here.
+class New_User_Form_Test(TestCase):
+
+    # Valid Form Data
+    def test_NewUserForm_valid(self):
+    	info = {
+    		'username': 'testclient',
+    		'email': 'user@user.com',
+    		'library_card_number': 123456789,
+    		'password1': 'test123456',
+    		'password2': 'test123456',
+    	}
+    	form = NewUserForm(info)
+    	self.assertTrue(form.is_valid())
+
+    # Invalid Form Data
+    def test_NewUserForm_invalid(self):
+    	info = {
+    		'username': 'testclient',
+    		'email': 'user',
+    		'library_card_number': 1234242342,
+    		'password1': 'test',
+    		'password2': 'test',
+    	}
+    	form = NewUserForm(info)
+    	self.assertFalse(form.is_valid())
