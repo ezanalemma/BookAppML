15d4fb84d5e59d13e1035be5531d72f67d1c8fc5
Mannat Kaur <mannatk@gmail.com>
2020-05-01 16:29:41 -0700

    added test for DB

--- mysite/register/tests.py
+++ mysite/register/tests.py
@@ -5,6 +5,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout, authenticate
 from django_webtest import WebTest
 from selenium import webdriver
+from .views import register
 
 class New_User_Form_Test(TestCase):
 
@@ -32,6 +33,18 @@ class New_User_Form_Test(TestCase):
     	form = NewUserForm(info)
     	self.assertFalse(form.is_valid())
 
+    # check to see database updated
+    def test_database_updated(self):
+        info = {
+            'username': 'testclient',
+            'email': 'user@user.com',
+            'library_card_number': 123456789,
+            'password1': 'test123456',
+            'password2': 'test123456',
+        }
+        self.assertEqual(User.objects.count(), 0)
+        self.client.post('/register/', info)
+        self.assertEqual(User.objects.count(), 1)
 
 class Login_Test(TestCase):
 	def test_Login_valid(self):
@@ -56,8 +69,6 @@ class TestSignup(TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome()
-
-    def test_signup(self):
         self.driver.get("http://127.0.0.1:8000/register")
         element1 = self.driver.find_element_by_id("id_username")
         element1.send_keys("testing123")
@@ -68,6 +79,7 @@ class TestSignup(TestCase):
         element3 = self.driver.find_element_by_id("id_library_card_number")
         element3.send_keys("123456789")
 
+    def test_signup(self):
         element4 = self.driver.find_element_by_id("id_password1")
         element4.send_keys("selenium")
 
@@ -82,16 +94,6 @@ class TestSignup(TestCase):
         self.assertEquals(element7.text, "testing123")
 
     def test_invalid_signup(self):
-        self.driver.get("http://127.0.0.1:8000/register")
-        element1 = self.driver.find_element_by_id("id_username")
-        element1.send_keys("testing123")
-
-        element2 = self.driver.find_element_by_id("id_email")
-        element2.send_keys("testing@testing.com")
-
-        element3 = self.driver.find_element_by_id("id_library_card_number")
-        element3.send_keys("123456789")
-
         element4 = self.driver.find_element_by_id("id_password1")
         element4.send_keys("selenium123")
 
@@ -105,27 +107,54 @@ class TestSignup(TestCase):
         src = self.driver.page_source
         self.assertTrue('password_mismatch' in src)
 
-    def test_login_logout(self):
+    def tearDown(self):
+        self.driver.quit
+
+class TestLogin(TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
         self.driver.get("http://127.0.0.1:8000/register")
-        element = self.driver.find_element_by_id("have_login")
-        element.click()
-        self.assertEquals(self.driver.current_url, "http://127.0.0.1:8000/login")
         element1 = self.driver.find_element_by_id("id_username")
-        element1.send_keys("testing123")
+        element1.send_keys("testing1234")
+
+        element2 = self.driver.find_element_by_id("id_email")
+        element2.send_keys("testing1@testing.com")
+
+        element3 = self.driver.find_element_by_id("id_library_card_number")
+        element3.send_keys("123456789")
+        element4 = self.driver.find_element_by_id("id_password1")
+        element4.send_keys("selenium1")
+
+        element5 = self.driver.find_element_by_id("id_password2")
+        element5.send_keys("selenium1")
+
+        element6 = self.driver.find_element_by_id("btn")
+        element6.click()
+
+    def test_login_logout(self):
+
+        self.driver.get("http://127.0.0.1:8000/login")
+
+        element1 = self.driver.find_element_by_id("id_username")
+        element1.send_keys("testing1234")
         element4 = self.driver.find_element_by_id("id_password")
-        element4.send_keys("selenium")
+        element4.send_keys("selenium1")
         element6 = self.driver.find_element_by_id("log")
         element6.click()
+
         self.assertEquals(self.driver.current_url, "http://127.0.0.1:8000/home/")
         element7 = self.driver.find_element_by_id("user")
-        self.assertEquals(element7.text, "testing123")
-        element = self.driver.find_element_by_id("logout")
-        element.click()
-        self.assertEquals(self.driver.current_url, "http://127.0.0.1:8000/register")
-        
+        self.assertEquals(element7.text, "testing1234")
+
+        element8 = self.driver.find_element_by_id("logout")
+        element8.click()
+        self.assertEquals(self.driver.current_url, "http://127.0.0.1:8000/register/")
 
     def tearDown(self):
         self.driver.quit
+
+    
 
 if __name__ == '__main__':
     unittest.main()
