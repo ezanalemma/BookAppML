0a6bf6b0fa91f0ca16090c524463f3557cb7a7a7
Mannat Kaur <mannatk@gmail.com>
2020-05-10 18:53:23 -0700

    uploaded user-book-rating csv

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -42,46 +42,8 @@ class Book(models.Model):
         return self.image_url;
 
 class Rating(models.Model):
-    user_id = models.IntegerField(default=0);
-    art =  models.IntegerField(default=0);
-    biography =  models.IntegerField(default=0);
-    business =  models.IntegerField(default=0);
-    chick_lit =  models.IntegerField(default=0);
-    childrens =  models.IntegerField(default=0);
-    chritstian =  models.IntegerField(default=0);
-    classics =  models.IntegerField(default=0);
-    comics =  models.IntegerField(default=0);
-    contemporary =  models.IntegerField(default=0);
-    cookbooks =  models.IntegerField(default=0);
-    crime =  models.IntegerField(default=0);
-    ebooks =  models.IntegerField(default=0);
-    fantasy =  models.IntegerField(default=0);
-    fiction =  models.IntegerField(default=0);
-    gay_lesbian =  models.IntegerField(default=0);
-    graphic_novels =  models.IntegerField(default=0);
-    historical_fiction =  models.IntegerField(default=0);
-    history =  models.IntegerField(default=0);
-    horror =  models.IntegerField(default=0);
-    humor =  models.IntegerField(default=0);
-    manga =  models.IntegerField(default=0);
-    memoir =  models.IntegerField(default=0);
-    music =  models.IntegerField(default=0);
-    mystery =  models.IntegerField(default=0);
-    nonfiction =  models.IntegerField(default=0);
-    paranormal =  models.IntegerField(default=0);
-    philosophy =  models.IntegerField(default=0);
-    poetry =  models.IntegerField(default=0);
-    psychology =  models.IntegerField(default=0);
-    religion =  models.IntegerField(default=0);
-    romance =  models.IntegerField(default=0);
-    science =  models.IntegerField(default=0);
-    science_fiction =  models.IntegerField(default=0);
-    self_help =  models.IntegerField(default=0);
-    suspense =  models.IntegerField(default=0);
-    spirituality =  models.IntegerField(default=0);
-    sports =  models.IntegerField(default=0);
-    thriller =  models.IntegerField(default=0);
-    travel =  models.IntegerField(default=0);
-    young_adult =  models.IntegerField(default=0);
+    user_id = models.IntegerField(default=0)
+    book_id = models.IntegerField(default =0)
+    rating = models.IntegerField(default=0)
 
 	
